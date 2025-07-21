f9e88540a96a6f57d84bbf8a2b43e092c8c7a19d
RamCodeZ3 <arammusset7@gmail.com>
2025-07-21 17:18:02 -0400

    se creo el componente GetUserAll para mostrar todos los usuarios almacenado en la base de datos.

--- src/component/GetUserAll.jsx
+++ src/component/GetUserAll.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
-function ListUsers(){
+function GetUserAll(){
     const [users, setUsers] = useState([])
     const [loading, setloading] = useState(true);
     const [error, setError] = useState(null)
     const navigate = useNavigate()
 
     useEffect(()=>{
-        fetch("http://localhost:3000/usersrandom")
+        fetch("http://localhost:3000/users")
         .then(response=>{
             if(!response.ok){
                 throw new Error("Hubo un error al momento de obtener los datos")
@@ -27,19 +27,17 @@ function ListUsers(){
 
     if (loading) return <span>Cargando usuarios</span>
     if (error) return <span>Error: {error}</span>
-    
     return(
         <>
-        <div className="grid grid-cols-4 gap-2 rounded-lg p-4 max-w-[1000px] mx-auto h-[500px] justify-items-center">
+         <div className="grid grid-cols-12 grid-rows-10 gap-2 rounded-lg max-w-[1200px] mx-auto min-h-[500px]">
             {users.map(user =>(
-                <div className={`flex flex-col gap-1.5 items-center border-1 rounded-lg py-2 px-1 cursor-pointer transition-all duration-300 hover:scale-105
+                <div className={`flex items-center justify-center border-1 rounded-lg px-1 py-2 cursor-pointer transition-all max-h-[100px] w-auto duration-300 hover:scale-105
                     ${user.sex == "M" ? 'bg-[#287eff1c] border-[#0051FF] hover:bg-[#287eff50] ' : 'bg-[#ff3ba718] border-[#FF1194] hover:bg-[#ff3ba749]'}` } 
                 onClick={()=>navigate(`/user/${user.id}`)}>
                     <img 
                      src={user.url_img}
-                     className="w-[80%] h-auto rounded-lg "
+                     className="w-[90%] h-auto rounded-lg "
                      alt="" />
-                    <span className="font-semibold text-xs">{user.name} {user.lastname}</span>
                 </div>
             ))}
         </div>
@@ -47,4 +45,4 @@ function ListUsers(){
     )
 }
 
-export default ListUsers;
+export default GetUserAll;
