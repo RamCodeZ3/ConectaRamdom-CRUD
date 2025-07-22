b77455bd37a59df8d09ace7c04aab0637c8bd11b
RamCodeZ3 <arammusset7@gmail.com>
2025-07-22 16:31:38 -0400

    Se mejoro el estilo de la lista de usuario

--- src/component/GetUserAll.jsx
+++ src/component/GetUserAll.jsx
@@ -29,18 +29,33 @@ function GetUserAll(){
     if (error) return <span>Error: {error}</span>
     return(
         <>
-         <div className="grid grid-cols-12 grid-rows-10 gap-2 rounded-lg max-w-[1200px] mx-auto min-h-[500px]">
-            {users.map(user =>(
-                <div className={`flex items-center justify-center border-1 rounded-lg px-1 py-2 cursor-pointer transition-all max-h-[100px] w-auto duration-300 hover:scale-105
-                    ${user.sex == "M" ? 'bg-[#287eff1c] border-[#0051FF] hover:bg-[#287eff50] ' : 'bg-[#ff3ba718] border-[#FF1194] hover:bg-[#ff3ba749]'}` } 
-                onClick={()=>navigate(`/user/${user.id}`)}>
-                    <img 
-                     src={user.url_img}
-                     className="w-[90%] h-auto rounded-lg "
-                     alt="" />
-                </div>
-            ))}
-        </div>
+         <div className="w-full mx-auto max-h-[500px] overflow-y-auto p-2 py-6 bg-[#7c26cd0a] border-[#543CDC] border-b-1 border-t-1">
+            <div className="flex w-full justify-center py-2 h-auto items-center">
+                <span className="font-semibold text-3xl text-transparent bg-clip-text" style={{
+           backgroundImage: 'linear-gradient(to right, #0051FF, #FF1194)'
+         }}>Lista de usuarios</span>
+            </div>
+      <div className="grid grid-cols-12 gap-2">
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className={`flex items-center justify-center border rounded-lg px-1 py-2 cursor-pointer transition-all max-h-[100px] w-auto duration-300 hover:scale-105
+              ${
+                user.sex == "M"
+                  ? "bg-[#287eff1c] border-[#0051FF] hover:bg-[#287eff50]"
+                  : "bg-[#ff3ba718] border-[#FF1194] hover:bg-[#ff3ba749]"
+              }`}
+            onClick={() => navigate(`/user/${user.id}`)}
+          >
+            <img
+              src={user.url_img}
+              className="w-[85%] h-auto rounded-lg"
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+    </div>
         </>
     )
 }
